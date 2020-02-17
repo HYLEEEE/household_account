31850eca7428f8c094392965e929c87db5934f46
HYLEE <hywkd3@naver.com>
2020-02-17 09:11:09 +0900

    기능: 사이드바 개발

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('respond with a resource');
+
+  res.render('index', { title: 'Household Account', body: 'body' });
 });
+
+/*
+router.get('/withdrawal', function(req, res, next) {
+  // 입출금
+  res.render('withdrawal', { title: 'Household Account', body: 'withdrawal' });
+});
+
+router.get('/withdrawal/:userId/name', function(req, res, next) {
+  // 입출금
+  res.render('withdrawal', { title: 'Household Account', body: 'withdrawal', request: req.params });
+});
+*/
 
 module.exports = router;
