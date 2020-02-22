e479078543a28b17f4b68205858e8e1af5cddc11
HYLEE <hywkd3@naver.com>
2020-02-23 02:17:45 +0900

    기능: routing 추가

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Household Account', body: 'body' });
+  res.render('index', { title: 'Household Account', body: 'home' });
 });
 
-/*
 router.get('/withdrawal', function(req, res, next) {
   // 입출금
-  res.render('withdrawal', { title: 'Household Account', body: 'withdrawal' });
+  res.render('index', { title: 'Household Account - withdrawal', body: 'withdrawal' });
 });
 
 router.get('/withdrawal/:userId/name', function(req, res, next) {
   // 입출금
-  res.render('withdrawal', { title: 'Household Account', body: 'withdrawal', request: req.params });
+  res.render('index', { title: 'Household Account - withdrawal', body: 'withdrawal', request: req.params });
 });
-*/
 
 module.exports = router;
