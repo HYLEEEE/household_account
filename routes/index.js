44114a5388b36aaa485a71fc0f92c760787fc065
HYLEE <hywkd3@naver.com>
2020-02-23 22:48:37 +0900

    기능: 사이드바 라우팅 추가

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,36 @@ router.get('/', function(req, res, next) {
 
 router.get('/withdrawal', function(req, res, next) {
   // 입출금
-  res.render('index', { title: 'Household Account - withdrawal', body: 'withdrawal' });
+  res.render('index', { title: 'withdrawal', body: 'withdrawal' });
 });
 
+router.get('/account', function(req, res, next) {
+  // 계좌 리스트
+  res.render('index', { title: 'account', body: 'account' });
+});
+
+router.get('/saving', function(req, res, next) {
+  // 적금
+  res.render('index', { title: 'saving', body: 'saving' });
+});
+
+router.get('/standard', function(req, res, next) {
+  // 기준 항목
+  res.render('index', { title: 'standard', body: 'standard' });
+});
+
+router.get('/asset', function(req, res, next) {
+  // 자산현황
+  res.render('index', { title: 'asset', body: 'asset' });
+});
+
+
+/*
 router.get('/withdrawal/:userId/name', function(req, res, next) {
   // 입출금
   res.render('index', { title: 'Household Account - withdrawal', body: 'withdrawal', request: req.params });
 });
+*/
+
 
 module.exports = router;
