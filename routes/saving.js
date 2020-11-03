fe8bc7c2110dd6fb47c4f175c68823eb168b524d
hywkd3 <58063144+hywkd3@users.noreply.github.com>
2020-11-03 09:39:09 +0900

    적금 추가수정

--- routes/saving.js
+++ routes/saving.js
@@ -7,10 +7,10 @@ var connection = mysql_dbc.init();
 
 
 router.get('/', function (req, res, next) {
-    // 적금
-//    res.render('index', { title: 'saving', body: 'saving' });
+  // 적금
+  //    res.render('index', { title: 'saving', body: 'saving' });
 
-    var sql = 'SELECT `savings`.*, \
+  var sql = 'SELECT `savings`.*, \
     date_format(`savings`.expiration_date, "%Y-%m-%d") AS `expiration_date`, \
     `bank_accounts`.`name` AS `bank_account_name`, `bank_accounts`.`account_number`, `bank_accounts`.`bank_name`, \
     (SELECT SUM(price) FROM `saving_data` WHERE saving_id=`savings`.id) AS total_price, \
@@ -19,18 +19,43 @@ router.get('/', function (req, res, next) {
     JOIN `bank_accounts` ON `savings`.`expend_bank_account_id`=`bank_accounts`.`id` \
     ORDER BY `id` DESC';
 
-    connection.query(sql, function (error, results, fields) {
-      if (error) {
-        console.log(error);
-      }
-      console.log(sql);
-      console.log(results);
-      res.render('index', { title: 'saving', body: 'saving', dataList: results });
-      //    res.send(ejs.render(data, {prodList: results}));    
-    });
-
+  connection.query(sql, function (error, results, fields) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(sql);
+    console.log(results);
+    res.render('index', { title: 'saving', body: 'saving', dataList: results });
+    //    res.send(ejs.render(data, {prodList: results}));    
   });
 
+});
+
+router.get('/add', function (req, res, next) {
+  // 적금
+  //    res.render('index', { title: 'saving', body: 'saving' });
+  
+
+  var sql = 'SELECT `savings`.*, \
+    date_format(`savings`.expiration_date, "%Y-%m-%d") AS `expiration_date`, \
+    `bank_accounts`.`name` AS `bank_account_name`, `bank_accounts`.`account_number`, `bank_accounts`.`bank_name`, \
+    (SELECT SUM(price) FROM `saving_data` WHERE saving_id=`savings`.id) AS total_price, \
+    (SELECT SUM(price) FROM `saving_data` WHERE saving_id=`savings`.id AND YEAR(date)=YEAR(NOW())) AS year_total_price \
+    FROM `savings` \
+    JOIN `bank_accounts` ON `savings`.`expend_bank_account_id`=`bank_accounts`.`id` \
+    ORDER BY `id` DESC';
+
+  connection.query(sql, function (error, results, fields) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(sql);
+    console.log(results);
+    res.render('index', { title: 'saving', body: 'saving', dataList: results });
+    //    res.send(ejs.render(data, {prodList: results}));    
+  });
+
+});
 
 module.exports = router;
 
